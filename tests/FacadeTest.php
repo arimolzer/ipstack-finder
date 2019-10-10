cded524b7f81c84a27f72e3ae33f0def2bfd1bca
Ari Molzer <ari.molzer@dps.com.au>
2019-10-10 11:45:18 +1030

    Updated tests Updated composer dependancies

--- tests/FacadeTest.php
+++ tests/FacadeTest.php
@@ -11,17 +11,21 @@ use Arimolzer\IPStackFinder\Facade\IPStackFinderFacade;
 class FacadeTest extends TestCase
 {
     /** @test */
-    public function testFacade() : void
+    public function testConfigValues(): void
+    {
+        // Test an API key has been configured
+        $this->assertNotNull(config('ipstack-finder'), "No config");
+        $this->assertNotNull(config('ipstack-finder.api_key'), "Couldn't Load .env");
+    }
+
+    /** @test */
+    public function testFacade(): void
     {
         /**
          * Lets make a request to ipstack using Googles public DNS IP
          * @var array $data
          */
         $data = IPStackFinderFacade::get('8.8.8.8');
-
-        // Test an API key has been configured
-        $this->assertNotNull(config('ipstack-finder'), "No config");
-        $this->assertNotNull(config('ipstack-finder.api_key'), "Couldn't Load .env");
 
         // Check if you are getting a success response from the API
         $this->assertFalse(isset($data['success']), "ipstack.com has returned a failure response.");
